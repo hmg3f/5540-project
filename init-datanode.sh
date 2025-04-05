e42e2e2a56e1b0f97ddda54a8bf6a1dd8c763aa6
hmg3f <hmg3f@umsystem.edu>
2025-04-04 20:10:54 -0500

    increase log threshold
    increased logging threshold to WARN for zookeeper and hdfs. added gitattributes to set default EOL to LF. removed old script

--- init-datanode.sh
+++ init-datanode.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-rm -rf /opt/hadoop/data/datanode/*
+#rm -rf /opt/hadoop/data/datanode/*
+find /opt/hadoop/data/datanode/* ! -name 'README' -exec rm -rf {} +
 chown -R hadoop:hadoop /opt/hadoop/data/datanode
 chmod 755 /opt/hadoop/data/datanode
 hdfs datanode
--- reset.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo rm -rf ./hadoop_namenode/*  
-sudo rm -rf ./hadoop_datanode_1/*
