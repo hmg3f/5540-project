addef6c5e1326f825213083a697294d419c03bae
hmg3f <hmg3f@umsystem.edu>
2025-04-15 13:58:42 -0500

    automate kafka topics
    automatically assign kafka topics, add datasets

--- kafka_topics.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/opt/kafka/bin/kafka-topics.sh --create --topic social_media --partitions 1 --replication-factor 1 --bootstrap-server broker-1:19092
-/opt/kafka/bin/kafka-topics.sh --create --topic openkc_data --partitions 1 --replication-factor 1 --bootstrap-server broker-1:19092
-/opt/kafka/bin/kafka-topics.sh --create --topic shot_spotter --partitions 1 --replication-factor 1 --bootstrap-server broker-1:19092
